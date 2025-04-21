48e22dfc7d57b2036242570185cb5039a59f3cc6
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-04-21 11:06:57 +0300

    Update users.ts

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,22 +1,37 @@
 import api from './axios';
 
-export const getUsers = () =>
-  api.get('/users').then(res => res.data);
+export const getUsers = async () => {
+  const response = await api.get('/users');
+  return response.data;
+};
 
-export const getUserById = (id: string) =>
-  api.get(`/users/${id}`).then(res => res.data);
+export const getUserById = async (id: string | number) => {
+  const response = await api.get(`/users/${id}`);
+  return response.data;
+};
 
-export const getUserPosts = (id: string) =>
-  api.get(`/posts?userId=${id}`).then(res => res.data);
+export const getUserPosts = async (id: string | number) => {
+  const response = await api.get(`/users/${id}/posts`);
+  return response.data;
+};
 
-export const getUserAlbums = (id: string) =>
-  api.get(`/albums?userId=${id}`).then(res => res.data);
+export const getUserAlbums = async (id: string | number) => {
+  const response = await api.get(`/users/${id}/albums`);
+  return response.data;
+};
 
-export const getUserTodos = (id: string) =>
-  api.get(`/todos?userId=${id}`).then(res => res.data);
+export const getUserTodos = async (id: string | number) => {
+  const response = await api.get(`/users/${id}/todos`);
+  return response.data;
+};
 
-export const getPhotosByAlbumId = (id: string) =>
-  api.get(`/photos?albumId=${id}`).then(res => res.data);
+export const getPhotosByAlbumId = async (albumId: string | number) => {
+  const response = await api.get(`/albums/${albumId}/photos`);
+  return response.data;
+};
 
-export const getPostsByUserId = (id: string) =>
-  api.get(`/posts?userId=${id}`).then(res => res.data);
+export const getPostsByUserId = async (userId: string | number) => {
+  const response = await api.get(`/users/${userId}/posts`);
+  return response.data;
+};
+
