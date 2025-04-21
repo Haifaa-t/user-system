f0c08454ebe1e5f27335d382a73eb3fe813baf57
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-04-21 11:11:24 +0300

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,37 +1,42 @@
 import { Link } from 'react-router-dom';
 
-type UserCardProps = {
+type User = {
   id: number;
   name: string;
   email: string;
   phone: string;
-  street: string;
-  company: string;
+  address: {
+    street: string;
+  };
+  company: {
+    name: string;
+  };
 };
 
-const UserCard = ({ id, name, email, phone, street, company }: UserCardProps) => (
-  <li
-    className="bg-white border-l-4 border-yellow-400 p-6 rounded-lg shadow hover:shadow-lg transition duration-300"
-  >
-    <Link
-      to={`/users/${id}`}
-      className="text-red-600 font-bold text-xl hover:underline block mb-2"
-    >
-      {name}
-    </Link>
-    <p className="text-gray-700">
-      <strong className="text-gray-900">Email:</strong> {email}
-    </p>
-    <p className="text-gray-700">
-      <strong className="text-gray-900">Phone:</strong> {phone}
-    </p>
-    <p className="text-gray-700">
-      <strong className="text-gray-900">Street:</strong> {street}
-    </p>
-    <p className="text-gray-700">
-      <strong className="text-gray-900">Company:</strong> {company}
-    </p>
-  </li>
-);
+const UserCard = ({ user }: { user: User }) => {
+  return (
+    <div className="bg-white border-l-4 border-yellow-400 p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
+      <Link
+        to={`/users/${user.id}`}
+        className="text-red-600 font-bold text-xl hover:underline block mb-2"
+      >
+        {user.name}
+      </Link>
+      <p className="text-gray-700">
+        <strong className="text-gray-900">Email:</strong> {user.email}
+      </p>
+      <p className="text-gray-700">
+        <strong className="text-gray-900">Phone:</strong> {user.phone}
+      </p>
+      <p className="text-gray-700">
+        <strong className="text-gray-900">Street:</strong> {user.address.street}
+      </p>
+      <p className="text-gray-700">
+        <strong className="text-gray-900">Company:</strong> {user.company.name}
+      </p>
+    </div>
+  );
+};
 
 export default UserCard;
+
